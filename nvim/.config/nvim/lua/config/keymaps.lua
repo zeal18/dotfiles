45f57f5adc21a81790fa715c6f2d77e3c7320764
Aleksei Lezhoev <lezhoev@gmail.com>
2023-10-18 14:56:22 +0200

    [nvim] Add keymaps to copy current file path

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set(
+  { "n", "x", "v" },
+  "<leader>fCp",
+  "<cmd>let @+ = expand('%')<cr><cmd>echo 'copied ' . expand('%')<cr>",
+  { desc = "Copy relative file path" }
+)
+
+vim.keymap.set(
+  { "n", "x", "v" },
+  "<leader>fCP",
+  "<cmd>let @+ = expand('%:p')<cr><cmd>echo 'copied ' . expand('%:p')<cr>",
+  { desc = "Copy absolute file path" }
+)
