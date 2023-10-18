00a72650b9dbcf8f4c8b39274a2dcb6b5b89cc45
Aleksei Lezhoev <lezhoev@gmail.com>
2023-10-18 16:29:08 +0200

    [wezterm] Set OneHalfDark theme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,21 +1,21 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = {}
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 config.hide_tab_bar_if_only_one_tab = true
 
-config.font = wezterm.font 'FiraCode Nerd Font'
-config.color_scheme = 'Atom'
-config.audible_bell = 'Disabled'
+config.font = wezterm.font("FiraCode Nerd Font")
+config.color_scheme = "OneHalfDark"
+config.audible_bell = "Disabled"
 config.window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
-  }
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
 
 return config
