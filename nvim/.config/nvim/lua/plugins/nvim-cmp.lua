9b73815acaf306d6610da847eb04b25735924e85
Aleksei Lezhoev <lezhoev@gmail.com>
2024-01-12 18:01:47 +0100

    Set 'insert' behaviour for cmp-confirm action

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -25,7 +25,7 @@ return {
           -- is no vim docs, but you can't have select = true here _unless_ you are
           -- also using the snippet stuff. So keep in mind that if you remove
           -- snippets you need to remove this select
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
           -- I use tabs... some say you should stick to ins-completion but this is just here as an example
           ["<Tab>"] = function(fallback)
             if cmp.visible() then
