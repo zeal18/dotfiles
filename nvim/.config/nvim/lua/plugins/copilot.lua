e3e4ceb5561eff67e62b9cc745603bc237fb8e80
Aleksei Lezhoev <lezhoev@gmail.com>
2024-05-22 13:00:27 +0200

    [nvim] Drop nvim-cmp config from copilot

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -81,18 +81,4 @@ return {
       end)
     end,
   },
-
-  --   "nvim-cmp",
-  --   dependencies = {
-  --     ,
-  --   },
-  --   ---@param opts cmp.ConfigSchema
-  --   opts = function(_, opts)
-  --     table.insert(opts.sources, 1, {
-  --       name = "copilot",
-  --       group_index = 1,
-  --       priority = 100,
-  --     })
-  --   end,
-  -- },
 }
