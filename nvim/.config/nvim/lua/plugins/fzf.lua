2d5f05865fc4548f1d427dc3cd39d30ef3fec7f9
Aleksei Lesonen <lezhoev@gmail.com>
2025-10-07 15:33:18 +0200

    [nvim] Enable fzf files line query

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -5,6 +5,9 @@ return {
       opts.previewers.builtin = vim.tbl_deep_extend("force", opts.previewers.builtin or {}, {
         snacks_image = { enabled = false },
       })
+      opts.files = vim.tbl_deep_extend("force", opts.files or {}, {
+        line_query = true,
+      })
     end,
   },
 }
